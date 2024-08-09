5a183b55442987f337d6de91d6d854ea96fdf050
Oğuz Timur Taşdelen <oguztasdelen96@gmail.com>
2024-08-10 01:43:04 +0300

    config file added

---
+++ src/config/configuration.ts
@@ -0,0 +1,17 @@
+export default () => ({
+    system: {
+        environment: process.env.ENVIRONMENT || 'DEVELOPMENT',
+        app_version: '1.0', 
+    },
+    server: {
+        url: process.env.SERVER_URL,
+        port: parseInt(process.env.PORT, 10) || 3000,
+    },
+    database: {
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        name: process.env.DATABASE_NAME,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD
+    }
+})
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { ValidationPipe } from '@nestjs/common';
+import configuration from './config/configuration';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Global Validation Pipe
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger - API Doc - localhost:3000/api
+  const swaggerEnvironments: Array<string> = ['DEVELOPMENT', 'ACCEPTANCE'];
+  if (swaggerEnvironments.includes(configuration().system.environment)) {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle('Dynamic Sports Lab - Swagger API Documentation')
+      .setDescription('REST API documentation of Dynamic Sports Lab')
+      .setVersion('1.0')
+      .build();
+    const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, swaggerDocument);
+  }
+  
+  await app.listen(configuration().server.port || 3000);
 }
 bootstrap();
